9493c59abcf3c66e0be7c82c1efd97a49c6261ec
CarlosSousa2001 <carlosramos10k@gmail.com>
2024-02-27 15:15:38 -0300

    criando modal de detalhes do pedido

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -4,20 +4,34 @@ import {
   TableCell,
   TableRow,
 } from "@/components/ui/table"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 import { ArrowRight, Search, X } from "lucide-react"
+import { OrderDetails } from "./order-details"
 
 // export interface OrderTableRowProps {
-  
+
 // }
 
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant="outline" size='xs'>
-          <Search className="h-3 w-3" />
-          <span className="sr-only">Detalhes do pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size='xs'>
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+            <OrderDetails/>
+        </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">dsgdfs7g4g5fd4g5fd4g6d</TableCell>
       <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
