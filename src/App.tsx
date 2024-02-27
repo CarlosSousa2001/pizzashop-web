f1c862d0d69a35b43aad2d17bb79da3d4dc0f035
CarlosSousa2001 <carlosramos10k@gmail.com>
2024-02-27 09:15:06 -0300

    configurando eslint e pretties -> intalando eslint modelo da rocketseat

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@ import { Button } from './components/ui/button'
 import './global.css'
 
 export function App() {
- 
+
   return (
-   <div className='flex justify-center items-center h-screen'>
-    <Button>Enviar</Button>
-   </div>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Enviar</Button>
+    </div>
   )
 }
-
