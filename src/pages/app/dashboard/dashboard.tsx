4e182c73988468c21e8618403e7c07befa18fb78
CarlosSousa2001 <carlosramos10k@gmail.com>
2024-02-27 15:52:36 -0300

    criando gráfico com recharts e configurando - obs função de pegar cores do tailwind pelo import

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,20 +4,25 @@ import { MouthRevenueCard } from './month-revenue-car'
 import { MouthOrdersAmountCard } from './moth-orders-amount-card'
 import { DaysOrdersAmount } from './day-orders-amount-card'
 import { MothCanceledOrdersAmount } from './month-canceled-orders-amount.-card'
+import { RevenueChart } from './revenue-chart'
 export function Dashboard() {
   return (
-    <div>
+    <>
       <Helmet title='Dashboard' />
       <div className='flex flex-col gap-4'>
         <h1 className='text-3xl font-bold tracking-tight'>Dashboard</h1>
 
         <div className='grid grid-cols-4 gap-4'>
-         <MouthRevenueCard/>
-         <MouthOrdersAmountCard/>
-         <DaysOrdersAmount/>
-         <MothCanceledOrdersAmount/>
+          <MouthRevenueCard />
+          <MouthOrdersAmountCard />
+          <DaysOrdersAmount />
+          <MothCanceledOrdersAmount />
+        </div>
+
+        <div className='grid grid-cols-9 gap-4'>
+            <RevenueChart/>
         </div>
       </div>
-    </div>
+    </>
   )
 }
