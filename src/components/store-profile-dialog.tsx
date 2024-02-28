0afecf26a7d4f9e173aa168e6de057a82c7845fd
CarlosSousa2001 <carlosramos10k@gmail.com>
2024-02-28 17:39:25 -0300

    avançando no react query

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { Button } from "./ui/button";
 import { DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "./ui/dialog";
 import { Input } from "./ui/input";
@@ -6,12 +6,15 @@ import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import { getManagerRestaurant } from "@/api/get-managed-restaurant";
 import { useForm } from "react-hook-form";
-import {z} from 'zod'
+import { z } from 'zod'
 import { zodResolver } from "@hookform/resolvers/zod";
+import { updateProfile } from "@/api/update-profile";
+import { toast } from "sonner";
+import { DialogClose } from "@radix-ui/react-dialog";
 
 const storeProfileSchema = z.object({
-    name: z.string(),
-    description: z.string()
+  name: z.string(),
+  description: z.string()
 })
 
 type StoreProfileData = z.infer<typeof storeProfileSchema>
@@ -19,19 +22,36 @@ export function StoreProfileDialog() {
 
   // devido o uso da chave  queryKey agora o reactQuery sabe que essa requisição ja foi feita antes
   // entao ele nao precisa fazer outra requisição, ele usa os dados da ultima requisição feita
-  const { data: managedRestaurant,} = useQuery({
+  const { data: managedRestaurant, } = useQuery({
     queryKey: ['managed-restaurant'],
     queryFn: getManagerRestaurant,
+    staleTime: Infinity
   })
 
-  const {register, handleSubmit} = useForm<StoreProfileData>({
+  const { register, handleSubmit, formState: { isSubmitted } } = useForm<StoreProfileData>({
     resolver: zodResolver(storeProfileSchema),
     // aqui precisamos usar o value, pois o default value usa os valores antes dos dados existirem
-    values:{
+    values: {
       name: managedRestaurant?.name ?? '',
       description: managedRestaurant?.description ?? ''
     }
   })
+
+  const { mutateAsync: updateProfileFn } = useMutation({
+    mutationFn: updateProfile,
+  })
+
+  async function handleDateProfile(data: StoreProfileData) {
+    try {
+      await updateProfileFn({
+        name: data.name,
+        description: data.description
+      })
+      toast.success('Perfil atualizado com sucessso')
+    } catch (error) {
+      toast.error('Falha ao atualizar o perfil')
+    }
+  }
 
 
 
@@ -41,20 +61,22 @@ export function StoreProfileDialog() {
         <DialogTitle>Perfil da Loja</DialogTitle>
         <DialogDescription>Atualize as informações do seu estabelecimento</DialogDescription>
       </DialogHeader>
-      <form >
+      <form onSubmit={handleSubmit(handleDateProfile)} >
         <div className="space-y-4 gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label className="text-right" htmlFor="name">Nome</Label>
-              <Input id="name" className="col-span-3" {...register('name')}/>
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label className="text-right" htmlFor="description">descrição</Label>
-              <Textarea id="description" className="col-span-3 resize-none" {...register('description')}/>
-            </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="name">Nome</Label>
+            <Input id="name" className="col-span-3" {...register('name')} />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="description">descrição</Label>
+            <Textarea id="description" className="col-span-3 resize-none" {...register('description')} />
+          </div>
         </div>
         <DialogFooter>
-          <Button variant="ghost" type="button">Cancelar</Button>
-          <Button type="submit" variant="success" >Salvar</Button>
+          <DialogClose>
+            <Button variant="ghost" type="button">Cancelar</Button>
+          </DialogClose>
+          <Button type="submit" variant="success" disabled={isSubmitted} >Salvar</Button>
         </DialogFooter>
       </form>
     </DialogContent>
