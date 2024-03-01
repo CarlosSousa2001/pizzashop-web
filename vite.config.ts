33ebca06ca6c67a5e5ba3935f202d4763496dab5
CarlosSousa2001 <carlosramos10k@gmail.com>
2024-03-01 14:48:11 -0300

    setup de teste

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
+import type { UserConfig } from 'vite'
+import type { InlineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +12,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  test: {
+    globals:true,
+    setupFiles: ['./test/setup.ts'],
+    enviroment: 'happy-dom'
+  }
+} as UserConfig & {
+  test: InlineConfig
 })
