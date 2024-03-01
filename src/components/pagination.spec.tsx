3beb6bad9644200ce0b173d6d603750123618198
CarlosSousa2001 <carlosramos10k@gmail.com>
2024-03-01 15:53:19 -0300

    teste componente nav-link

--- src/components/pagination.spec.tsx
+++ src/components/pagination.spec.tsx
@@ -5,6 +5,9 @@ import { userEvent } from '@testing-library/user-event'
 const onPageChangeCallBack = vi.fn()
 
 describe('Pagination', () => {
+  beforeEach(() => {
+    onPageChangeCallBack.mockClear()
+  })
   it('should diplay the rigth amount of pages an results', () => {
 
     const wrapper = render(<Pagination pageIndex={0} totalCount={200} perPage={10} onPageChange={() => { }} />)
@@ -29,6 +32,57 @@ describe('Pagination', () => {
     await user.click(nextPageButton)
 
     expect(onPageChangeCallBack).toHaveBeenCalledWith(1);
+
+
+  })
+  it('should be able to navigate to the previous page', async () => {
+
+
+    const user = userEvent.setup()
+    const wrapper = render(<Pagination pageIndex={5} totalCount={200} perPage={10} onPageChange={onPageChangeCallBack} />)
+
+    // procurando por um botão que tem o texto Próxima página
+    const nextPageButton = wrapper.getByRole('button', {
+      name: 'Página anterior'
+    })
+
+    await user.click(nextPageButton)
+
+    expect(onPageChangeCallBack).toHaveBeenCalledWith(4);
+
+
+  })
+  it('should be able to navigate to the first page', async () => {
+
+
+    const user = userEvent.setup()
+    const wrapper = render(<Pagination pageIndex={5} totalCount={200} perPage={10} onPageChange={onPageChangeCallBack} />)
+
+    // procurando por um botão que tem o texto Próxima página
+    const nextPageButton = wrapper.getByRole('button', {
+      name: 'Página anterior'
+    })
+
+    await user.click(nextPageButton)
+
+    expect(onPageChangeCallBack).toHaveBeenCalledWith(4);
+
+
+  })
+  it('should be able to navigate to the last page', async () => {
+
+
+    const user = userEvent.setup()
+    const wrapper = render(<Pagination pageIndex={0} totalCount={200} perPage={10} onPageChange={onPageChangeCallBack} />)
+
+    // procurando por um botão que tem o texto Próxima página
+    const nextPageButton = wrapper.getByRole('button', {
+      name: 'Última página'
+    })
+
+    await user.click(nextPageButton)
+
+    expect(onPageChangeCallBack).toHaveBeenCalledWith(19);
 
 
   })
